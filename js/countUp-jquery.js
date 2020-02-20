cdeba6cbed7e21895713a4f7970071197ba284ba
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-20 14:14:38 -0500

    renamed file

---
+++ js/countUp-jquery.js
@@ -0,0 +1,9 @@
+(function($){
+$.fn.countUp = fucntion(params){
+    if(typeof CountUp !== 'function') {
+        console.log(error)('countUp.js is a required dependency of countUp-jquery.js.');
+        return
+    }
+}
+
+})
